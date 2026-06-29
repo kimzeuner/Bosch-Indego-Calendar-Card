35d3e4a0ed75463e1560e976b87a7d5ab8a23620
kizeu <116279616+kimzeuner@users.noreply.github.com>
2026-06-29 11:10:56 +0200

    Add Indego Calendar Card to customCards

--- src/indego-calendar-card.js
+++ src/indego-calendar-card.js
@@ -1,1 +1,10 @@
+import "./editor.js";
+import "./card.js";
 
+window.customCards = window.customCards || [];
+window.customCards.push({
+  type: "indego-calendar-card",
+  name: "Indego Calendar Card",
+  description: "Bosch Indego calendar and SmartMowing schedule card",
+  preview: true,
+});
